754c5fda42bd4a85c9b3b424f905984ca47c70d8
teamgerama <teamgerama@gmail.com>
2026-06-01 02:27:03 +0000

    security: make debug dynamic and restrict production visibility

--- QUIZ1/settings.py
+++ QUIZ1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9#zrm%rmp)1mmqmh11yk+e)i+cbw-cmwuu)#_f20nz)ubx(3hy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'VERCEL' not in os.environ
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1', 'localhost', '*']
 
 # Application definition
 
