a415f91ed7e53da69731590bb3731675ef14ac5a
teamgerama <teamgerama@gmail.com>
2026-06-13 09:38:16 +0000

    Fix all

--- QUIZ1/wsgi.py
+++ QUIZ1/wsgi.py
@@ -1,18 +1,7 @@
-"""
-WSGI config for QUIZ1 project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QUIZ1.settings')
 
-application = get_wsgi_application()
-# Add this at the absolute bottom of QUIZ1/wsgi.py
-app = application
+# Vercel looks for 'app'
+app = get_wsgi_application()
